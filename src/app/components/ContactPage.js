030a8aca47ae12802bda7f12bcd9b013d925d0c6
humdrum <jjrajani@gmail.com>
2017-07-31 08:38:57 -0400

    changed contact page to a stateless component

--- src/app/components/ContactPage.js
+++ src/app/components/ContactPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { ContactForm } from '../containers';
 
-export default class ContactPage extends React.Component {
-  submit = (values) => {
-    console.log('submit form', values);
-  }
-  render() {
-    return (
-      <ContactForm onSubmit={this.submit} />
-    )
-  }
+let handleSubmit = (values) => {
+  console.log('submit form', values);
 }
+
+const ContactPage = () => (
+  <ContactForm onSubmit={handleSubmit} />
+);
+
+export default ContactPage;
